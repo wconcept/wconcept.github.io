38aa75f0a1f40905f5c611f6723649af9d92eced
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-11-23 16:51:35 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
-
 var _createClass = function () {function defineProperties(target, props) {for (var i = 0; i < props.length; i++) {var descriptor = props[i];descriptor.enumerable = descriptor.enumerable || false;descriptor.configurable = true;if ("value" in descriptor) descriptor.writable = true;Object.defineProperty(target, descriptor.key, descriptor);}}return function (Constructor, protoProps, staticProps) {if (protoProps) defineProperties(Constructor.prototype, protoProps);if (staticProps) defineProperties(Constructor, staticProps);return Constructor;};}();function _classCallCheck(instance, Constructor) {if (!(instance instanceof Constructor)) {throw new TypeError("Cannot call a class as a function");}}function _possibleConstructorReturn(self, call) {if (!self) {throw new ReferenceError("this hasn't been initialised - super() hasn't been called");}return call && (typeof call === "object" || typeof call === "function") ? call : self;}function _inherits(subClass, superClass) {if (typeof superClass !== "function" && superClass !== null) {throw new TypeError("Super expression must either be null or a function, not " + typeof superClass);}subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } });if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass;}var Header = function Header(props) {return (
     React.createElement("header", { className: "header" },
       React.createElement("div", { className: "header-logo" },
-        React.createElement("span", null, "Events"),
-        React.createElement("span", null, ""))));};
+        React.createElement("span", null, "Job Board"),
+        React.createElement("span", null, "The front-end design and development"))));};
+
 
 
 
@@ -66,7 +66,7 @@ App = function (_React$Component2) {_inherits(App, _React$Component2);
     } }, { key: "componentDidMount", value: function componentDidMount()
 
     {var _this3 = this;
-      axios.get('https://raw.githubusercontent.com/i-experience/i-experience.github.io/master/posts.json').
+      axios.get('https://codepen.io/jobs.json').
       then(function (response) {
         _this3.setState({
           jobs: response.data.jobs });
